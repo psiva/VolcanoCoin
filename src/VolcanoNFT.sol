40a37a0015898985a04d82b80d53bfa871782622
Siva Puvvada <siva.puvvada@gmail.com>
2022-11-03 19:06:58 +0530

    additional tests

--- src/VolcanoNFT.sol
+++ src/VolcanoNFT.sol
@@ -4,20 +4,29 @@ pragma solidity ^0.8.0;
 import "openzeppelin-contracts/access/Ownable.sol";
 import "openzeppelin-contracts/token/ERC721/ERC721.sol";
 import "openzeppelin-contracts/utils/Counters.sol";
-import "openzeppelin-contracts/utils/Base64.sol";
-import "openzeppelin-contracts/utils/Strings.sol";
+
+//import "openzeppelin-contracts/utils/Base64.sol";
+//import "openzeppelin-contracts/utils/Strings.sol";
 
 contract VolcanoNFT is ERC721, Ownable {
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIdCounter;
 
+    string myTokenURI;
+
     constructor() ERC721("VolcanoNFT", "VCO") {}
 
-    function safeMint(address _to) public payable onlyOwner returns (uint256) {
-        require(msg.value >= 0.01 ether);
+    function safeMint(address _to, string memory _tokenURI)
+        public
+        payable
+        onlyOwner
+        returns (uint256)
+    {
+        require(msg.value >= 0.01 ether, "VolcanoNFT:Insufficient Allowance");
         uint256 tokenId = _tokenIdCounter.current();
         _tokenIdCounter.increment();
         _safeMint(_to, tokenId);
+        myTokenURI = _tokenURI;
         return tokenId;
     }
 
@@ -29,15 +38,12 @@ contract VolcanoNFT is ERC721, Ownable {
         _transfer(_from, _to, _tokenId);
     }
 
-    /*function tokenURI(uint256 tokenId)
+    function tokenURI(uint256 tokenId)
         public
         view
         override
         returns (string memory)
     {
-        _requireMinted(tokenId);
-        string memory metadata;
-        return
-            string(abi.encodePacked("data:application/json;base64", metadata));
-    }*/
+        return myTokenURI;
+    }
 }
--- test/VolcanoNFT.t.sol
+++ test/VolcanoNFT.t.sol
@@ -9,6 +9,8 @@ contract VolcanoNFTTest is Test {
     address alice;
     address bob;
     address owner;
+    string public constant TOKEN_URI =
+        "ipfs://bafybeig37ioir76s7mg5oobetncojcm3c3hxasyd4rvid4jqhy4gkaheg4/?filename=0-PUG.json";
 
     /**
      * @dev This function creates the contract instance and a sample user
@@ -22,22 +24,39 @@ contract VolcanoNFTTest is Test {
     }
 
     function testMint_WhenOwner() public {
-        volcanoNFT.safeMint{value: 0.013 ether}(bob);
+        volcanoNFT.safeMint{value: 0.013 ether}(bob, TOKEN_URI);
         assertEq(volcanoNFT.balanceOf(bob), 1);
+    }
+
+    function testMint_WhenInsufficientAllowance() public {
+        vm.expectRevert("VolcanoNFT:Insufficient Allowance");
+        volcanoNFT.safeMint{value: 0.0013 ether}(bob, TOKEN_URI);
+        assertEq(volcanoNFT.balanceOf(bob), 0);
     }
 
     function testMint_WhenNotOwner() public {
         vm.startPrank(alice);
         vm.deal(alice, 1 ether);
         vm.expectRevert(bytes("Ownable: caller is not the owner"));
-        volcanoNFT.safeMint{value: 0.013 ether}(bob);
+        volcanoNFT.safeMint{value: 0.013 ether}(bob, TOKEN_URI);
         vm.stopPrank();
     }
 
     function testTransfer() public {
-        uint256 tokenId = volcanoNFT.safeMint{value: 0.013 ether}(bob);
+        uint256 tokenId = volcanoNFT.safeMint{value: 0.013 ether}(
+            bob,
+            TOKEN_URI
+        );
         volcanoNFT.transfer(bob, alice, tokenId);
         assertEq(volcanoNFT.balanceOf(bob), 0);
         assertEq(volcanoNFT.balanceOf(alice), 1);
+    }
+
+    function testTokenURI() public {
+        uint256 tokenId = volcanoNFT.safeMint{value: 0.013 ether}(
+            bob,
+            TOKEN_URI
+        );
+        assertEq(volcanoNFT.tokenURI(tokenId), TOKEN_URI);
     }
 }
