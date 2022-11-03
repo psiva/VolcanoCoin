0d5db9845949b7a48303d56ea8c1bc365e5edf2e
Siva Puvvada <siva.puvvada@gmail.com>
2022-11-03 17:21:27 +0530

    changes to test and made mint function payable

--- src/VolcanoNFT.sol
+++ src/VolcanoNFT.sol
@@ -4,6 +4,8 @@ pragma solidity ^0.8.0;
 import "openzeppelin-contracts/access/Ownable.sol";
 import "openzeppelin-contracts/token/ERC721/ERC721.sol";
 import "openzeppelin-contracts/utils/Counters.sol";
+import "openzeppelin-contracts/utils/Base64.sol";
+import "openzeppelin-contracts/utils/Strings.sol";
 
 contract VolcanoNFT is ERC721, Ownable {
     using Counters for Counters.Counter;
@@ -11,10 +13,12 @@ contract VolcanoNFT is ERC721, Ownable {
 
     constructor() ERC721("VolcanoNFT", "VCO") {}
 
-    function safeMint(address _to) public onlyOwner {
+    function safeMint(address _to) public payable onlyOwner returns (uint256) {
+        require(msg.value >= 0.01 ether);
         uint256 tokenId = _tokenIdCounter.current();
         _tokenIdCounter.increment();
         _safeMint(_to, tokenId);
+        return tokenId;
     }
 
     function transfer(
@@ -24,4 +28,16 @@ contract VolcanoNFT is ERC721, Ownable {
     ) public {
         _transfer(_from, _to, _tokenId);
     }
+
+    /*function tokenURI(uint256 tokenId)
+        public
+        view
+        override
+        returns (string memory)
+    {
+        _requireMinted(tokenId);
+        string memory metadata;
+        return
+            string(abi.encodePacked("data:application/json;base64", metadata));
+    }*/
 }
--- test/VolcanoNFT.t.sol
+++ test/VolcanoNFT.t.sol
@@ -8,22 +8,36 @@ contract VolcanoNFTTest is Test {
     VolcanoNFT public volcanoNFT;
     address alice;
     address bob;
+    address owner;
 
+    /**
+     * @dev This function creates the contract instance and a sample user
+     */
     function setUp() public {
         volcanoNFT = new VolcanoNFT();
+        owner = msg.sender;
+        vm.deal(owner, 1 ether);
         alice = vm.addr(1);
         bob = vm.addr(2);
     }
 
     function testMint_WhenOwner() public {
-        volcanoNFT.safeMint(bob);
+        volcanoNFT.safeMint{value: 0.013 ether}(bob);
         assertEq(volcanoNFT.balanceOf(bob), 1);
     }
 
     function testMint_WhenNotOwner() public {
         vm.startPrank(alice);
-        vm.expectRevert("Ownable: caller is not the owner");
-        volcanoNFT.safeMint(bob);
+        vm.deal(alice, 1 ether);
+        vm.expectRevert(bytes("Ownable: caller is not the owner"));
+        volcanoNFT.safeMint{value: 0.013 ether}(bob);
         vm.stopPrank();
+    }
+
+    function testTransfer() public {
+        uint256 tokenId = volcanoNFT.safeMint{value: 0.013 ether}(bob);
+        volcanoNFT.transfer(bob, alice, tokenId);
+        assertEq(volcanoNFT.balanceOf(bob), 0);
+        assertEq(volcanoNFT.balanceOf(alice), 1);
     }
 }
